8320959427d4cd296d0542668a9458a10745b0af
DrewXIII <andrew_XIII@hotmail.com>
2019-04-15 10:54:20 +0200

    new update

--- modulo02-JAVASCRIPT/Exercises/freeCodeCamp/Basic Algorithm Scripting/02.Reverse a String.js
+++ modulo02-JAVASCRIPT/Exercises/freeCodeCamp/Basic Algorithm Scripting/02.Reverse a String.js
@@ -7,10 +7,7 @@ You may need to turn the string into an array before you can reverse it.
 Your result must be a string.*/
 
 function reverseString(str) {
-  return str
-    .split("")
-    .reverse()
-    .join("");
+  return str.split("").reverse().join("");
 }
 
 reverseString("hello");
