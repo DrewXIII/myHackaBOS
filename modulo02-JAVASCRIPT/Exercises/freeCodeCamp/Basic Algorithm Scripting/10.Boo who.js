8bbb0313fc8de2eb136b3269bd495e8baf3cb2b7
DrewXIII <andrew_XIII@hotmail.com>
2019-04-15 12:16:55 +0200

    update

--- modulo02-JAVASCRIPT/Exercises/freeCodeCamp/Basic Algorithm Scripting/10.Boo who.js
+++ modulo02-JAVASCRIPT/Exercises/freeCodeCamp/Basic Algorithm Scripting/10.Boo who.js
@@ -6,7 +6,9 @@ Boolean primitives are true and false.*/
 
 function booWho(bool) {
   // What is the new fad diet for ghost developers? The Boolean.
-  return bool;
+
+  return typeof bool === 'boolean';
+
 }
 
 booWho(null);
