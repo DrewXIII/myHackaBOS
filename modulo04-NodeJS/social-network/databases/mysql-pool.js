d2480433910d52e676d460a59b1d22aa59747b74
DrewXIII <andrew_XIII@hotmail.com>
2019-04-23 16:57:23 +0200

    update

---
+++ modulo04-NodeJS/social-network/databases/mysql-pool.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const mysql = require('mysql2'); // "MySQL client for Node.js with focus on performance. Supports prepared statements, non-utf8 encodings, binary log protocol, compression, ssl ". Esta librería sirve para conecta con MySQL.
+
+async function connect() {
+
+}
--- modulo04-NodeJS/social-network/index.js
+++ modulo04-NodeJS/social-network/index.js
@@ -1,1 +1,11 @@
-const hola = hola;
+'use strict';
+
+const accountRouter = require('./webserver/routes/account-router'); // Con esto cogemos la variable accountRouter del archivo en el que la creamos.
+const bodyParser = require('body-parser'); // Node.js body parsing middleware.Parse incoming request bodies in a middleware before your handlers, available under the req.body property.
+const express = require('express');
+
+
+const app = express();
+app.use(bodyParser.json());
+
+app.use('/api', accountRouter);
